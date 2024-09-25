b016138c7c8f2c6f6c8b086d90d098208b5cce22
Daniel Daugherty <danielmasis11@gmail.com>
2024-09-25 10:19:21 -0700

    Added docstrings to functions and commented code for easy readability; added statistics reporting for mining trucks after teh simulation ends; code also now tracks total number of unloads a mining truck performs by incremementing a counter only when the unloading process has been completed

--- simulation.py
+++ simulation.py
@@ -1,14 +1,32 @@
 import argparse
 import random
+import datetime
 import constants as const
 
 class MiningTruck():
+    ''' Represents a mining truck that continuously performs the following work flow:
+            goToMiningLocation
+            startMining
+            goToUnloadStation
+            unload
 
-    def __init__(self, task_queue):
-        # each truck starts at a mining location - add a mining task to the task queue when a truck is instantiated
+        Attributes:
+            total_unloads (int): Number of times this truck unloaded it's mined Helium-3 at an unload station
+            total_times_mined (int): Number of times this truck mined Helium-3 at a mining location
+            time_spent_waiting (int): Total time in seconds this truck spent waiting at an unload station (in the event all stations were in use when the truck arrived)
+            time_spent_mining (int): Total time in seconds this truck spent mining at mining locations
+            times_traveled (int): Total number of times this truck traveled between mining locations and the unload station
+    '''
+
+    def __init__(self, id, task_queue):
+        ''' Initialize a MiningTruck '''
+        
+        self.id = id
+
+        # Each truck starts the simulation at a mining location - add a mining task to the 'task queue' upon the instantiation of a MiningTruck
         task_queue.enqueue((self.startMining, 0))
     
-        # Class attrs for tracking mining truck mining/unload statistics
+        # Variables for tracking a MiningTruck instance's statistics throughout the simulation
         self.total_unloads = 0
         self.total_times_mined = 0
         self.time_spent_waiting = 0
@@ -16,77 +34,130 @@ class MiningTruck():
         self.times_traveled = 0
 
     def goToMiningLocation(self, cur_time):
+        ''' Travel to a mining location from the unload station.
+
+            Args:
+                cur_time (int): Time at which the truck begins traveling to the mining location
+            Returns:
+                tuple (func, int): Tuple containing this truck's next task, and the time the task should be executed. This tuple will be added to the simulation's task queue.
+        '''
+        
         self.times_traveled += 1
+
         return((self.startMining, cur_time + const.TRAVEL_TIME))
     
     def goToUnloadStation(self, cur_time):
+        ''' Travel to an unload station from the mining location after a mining operation has been completed.
+
+            Args:
+                cur_time (int): Time at which the truck begins traveling to the unload station
+            Returns:
+                tuple (func, int): Tuple containing this truck's next task, and the time the task should be executed. This tuple will be added to the simulation's task queue.
+        '''
+
         self.times_traveled += 1
+        
         return((self.unload, cur_time + const.TRAVEL_TIME))
     
     def startMining(self, cur_time):
-        # Mining trucks spend a random amount of time between 1 and 5 hours at mining locations
-        # Get a random time between 1 and 5 hours in seconds 
+        ''' Start the mining process for this truck. Mining time is a random time in seconds between 1 and 5 hours.
+
+            Args:
+                cur_time (int): Time at which the truck begins the mining operation
+            Returns:
+                tuple (func, int): Tuple containing this truck's next task, and the time the task should be executed. This tuple will be added to the simulation's task queue.
+        '''
+
+        # Get a random time between 1 and 5 hours in seconds, representing the amount of time this truck will spend mining
         mining_time = random.randint(3600, 18000)
+        
         self.time_spent_mining += mining_time
         self.total_times_mined += 1
 
         return((self.goToUnloadStation, cur_time + mining_time))
     
     def unload(self, cur_time, station):
-        self.time_spent_waiting += station.queueTime(cur_time)
-        
-        station.enqueue(cur_time)
-        return (self.goToMiningLocation, cur_time + station.queueTime(cur_time))
+        ''' Start the unloading process of Helium-3 at the specified unload station.
+
+            Args:
+                cur_time (int): Time at which the truck begins traveling to the mining location
+                station (instance): Instance of the station this truck is currently unloading or waiting to unload at
+            Returns:
+                tuple (func, int): Tuple containing this truck's next task, and the time the task should be executed. This tuple will be added to the simulation's task queue.
+        '''
+        queue_time = station.queueTime(cur_time)
+        self.time_spent_waiting += queue_time
+       
+        station.enqueue(self, cur_time+queue_time)
+        return (self.goToMiningLocation, cur_time + queue_time)
+
 
 class UnloadStation():
-    
-    def __init__(self):
-        self.queue = []
+    ''' Represents an unload station where mining trucks unload Helium-3.
+
+        Attributes:
+            total_unloads (int): Number of times this truck unloaded it's mined Helium-3 at an unload station
+            total_times_mined (int): Number of times this truck mined Helium-3 at a mining location
+            time_spent_waiting (int): Total time in seconds this truck spent waiting at an unload station (in the event all stations were in use when the truck arrived)
+            time_spent_mining (int): Total time in seconds this truck spent mining at mining locations
+            times_traveled (int): Total number of times this truck traveled between mining locations and the unload station
+    '''
+
+    def __init__(self, id):
+        self.id = id
+        # This list contains the starting times for each truck currently at the station
+        self.truck_queue = []
+        # The start time of the truck currently unloading at the station
         self.start_time = None
     
+        # Variables for tracking unload station statistics throughout the simulation
         self.time_spent_waiting = 0
         self.total_unloads_completed = 0
 
-
     def inUse(self):
-        return len(self.queue) > 0
+        return len(self.truck_queue) > 0
     
     def queueTime(self, cur_time):
         if not self.inUse():
             return 0
-        return len(self.queue)*const.UNLOAD_TIME - (cur_time - self.start_time)
-    
-    def enqueue(self, cur_time):
+        return len(self.truck_queue)*const.UNLOAD_TIME - (cur_time - self.start_time)
+
+    def enqueue(self, truck, cur_time):
 
         self.time_spent_waiting += self.queueTime(cur_time)
 
         if not self.inUse():
             self.start_time = cur_time
    
-        self.queue.append(cur_time)
+        self.truck_queue.append((truck, cur_time))
     
     def startNextUnload(self, cur_time):
         
         self.total_unloads_completed += 1
 
-        self.queue = self.queue[1:]
+        self.truck_queue[0][0].total_unloads += 1
 
-        if len(self.queue):
-            self.start_time = self.queue[0]
+        self.truck_queue = self.truck_queue[1:]
+
+        if len(self.truck_queue):
+            self.start_time = self.truck_queue[0][1]
 
         return None
         
-        
+
 class TaskQueue():
 
     def __init__(self):
         self.queue = []
     
     def enqueue(self, task):
-        time = task[1]
+        func, time = task
         i = 0
         while i < len(self.queue):
             if time < self.queue[i][1]:
+                break
+            elif time == self.queue[i][1] and func.__name__ == 'startNextUnload':
+                # We want to make sure we clear 
                 break
             i += 1
         self.queue.insert(i, task)
@@ -97,28 +168,46 @@ class TaskQueue():
         return nextTask
 
 def getNextUnloadStation(stations, cur_time):
+
     mn = None
     mnStation = None
+
     for station in stations:
+        
+        # Return the first station that isn't currently in use
         if not station.inUse():
             return station
-        unload_time = station.queueTime(cur_time)
-        if not mn or unload_time < mn:
-            mn = unload_time
+
+        # Calculate the time it would take to unload at a station
+        queue_time = station.queueTime(cur_time)
+        
+        # Find the minimum time it would take to unload at a station in the event all stations are in use
+        if not mn or queue_time < mn:
+            mn = queue_time
             mnStation = station
+    
     return mnStation
 
 def RunSimulation(n, m):
-   
-    # Set up objects - add more description here
+    ''' Runs the simulation of a lunar Helium-3 mining operation. The simulation represents 72 hours of non-stop mining and must execute faster than real-time to provide timely analysis.
+        Provides statistics related to truck/unload station performance and efficiency upon simulation completion.
+
+        Args:
+            n (int): Number of mining trucks in the simulation.
+            m (int): Number of unload stations in the simulation.
+    '''
+
+    # Initialize instances of each mining truck/unload station and the task_queue
     task_queue = TaskQueue()
-    mining_trucks = [MiningTruck(task_queue) for i in range(n)]
-    unload_stations = [UnloadStation() for i in range(m)]
+    mining_trucks = [MiningTruck(i+1, task_queue) for i in range(n)]
+    unload_stations = [UnloadStation(i+1) for i in range(m)]
     
     while True:
-        # Pull next item from task queue; each item in the queue is a tuple in the form of (task, time)
+        # Continuously pull the next item from the task queue; each item in the queue is a tuple in the form of (task, time) - time is when the task starts executing
+        # No need to worry about the queue being empty, everytime a task is performed, the next task in the mining truck's task flow is added to the queue
         task, time = task_queue.getCurrentTask()
-        print(time, task.__name__)
+        #print(time, task.__name__, task.__self__.id)
+
         # Verify the simulation time hasn't passed the 72 hour mark; if it does, break
         if time > const.TOTAL_SIM_TIME:
             break
@@ -133,17 +222,23 @@ def RunSimulation(n, m):
         if nextTask:
             task_queue.enqueue(nextTask)
     
-    # Display results here
-    for i in mining_trucks:
-        print(i.time_spent_mining)
-        print(i.total_times_mined)
-
+    # Display statistics of the performance/efficiency of each mining truck and unload station
+    print("\nTruck ID | Total Mining Time | Average Mining Time | Total Unloads | Time Spent Waiting")
+    print("-"*87)
+    for i in sorted(mining_trucks, key=lambda x: x.total_unloads, reverse=True):
+        time_spent_mining = "{}h {}".format(str(i.time_spent_mining//3600), datetime.datetime.fromtimestamp(i.time_spent_mining).strftime('%Mm %Ss'))
+        time_spent_waiting = "{}h {}".format(str(i.time_spent_waiting//3600), datetime.datetime.fromtimestamp(i.time_spent_waiting).strftime('%Mm %Ss'))
+        print("{id:>8d} | {tmt:>17s} | {amt:>19.2f} | {tu:>13d} | {tsw:>18s}".format(id=i.id, tmt=time_spent_mining, amt=i.time_spent_mining/i.total_times_mined, tu=i.total_unloads, tsw=time_spent_waiting))
+        
 
 def ParseArgs():
-    # Parse arguments for number of mining trucks (n) and number of unload stations (m)
+    ''' Parse arguments using the argparse module for the number of mining trucks (n) and number of unload stations (m).
+        Both arguments are required and are non-positional, keyword arguments. '''
+
     parser = argparse.ArgumentParser(description='Get number of mining trucks and unload station')
     parser.add_argument('-n', '--numTrucks', type=int, help='Number of mining trucks', required=True)
     parser.add_argument('-m', '--unloadStations', type=int, help='Number of unload stations', required=True)
+    parser.add_argument('--logging', type=bool, help='Whether or not to log the mining truck tasks to a separate log file')
     args = parser.parse_args()
     return args.numTrucks, args.unloadStations
 
